a0997c181a11b9aaae70a6eba6ebd4030f3a91a3
Arthur Maurer <30410972+maurerarthur@users.noreply.github.com>
2020-08-05 01:36:59 -0300

    Users endpoint created

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const users = require("./routes/users.js");
 
 app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    res.redirect("/api");
+    res.redirect("/api/users");
 });
 
-app.use("/api", users);
+app.use("/api/users", users);
 
 app.listen(8000);
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,4 +1,5 @@
 const Router = require("express").Router();
+const cryptoJS = require("crypto-js/md5");
 const database = require("../ConnectionFactory.js");
 
 const Connection = new database();
@@ -7,12 +8,61 @@ Router.get("/", async (req, res) => {
 
     const conn = await Connection.Mongo();
 
-    console.log(conn);
+    conn.db("api").collection("users").find({}).toArray((err, result) => {
 
-    res.send({
-        id: 1289301283912,
-        name: "John",
-        email: "john@live.com"
+        if(err) {
+            throw err;
+        }
+
+        res.send(result);
+
+    });
+
+});
+
+Router.post("/", async (req, res) => {
+
+    const conn = await Connection.Mongo();
+
+    conn.db("api").collection("users").findOne({ email: req.body.email }, (err, result) => {
+
+        if(err) {
+            throw err;
+        }
+
+        if(result) {
+            res.send({
+                message: `The email ${req.body.email} has already been registered`,
+                status: 200
+            });
+        } else {
+
+            [req.body].forEach(i => {
+
+                if(i.name != null && i.email != null && i.password != null) {
+        
+                    conn.db("api").collection("users").insertOne({
+                        name: req.body.name,
+                        email: req.body.email,
+                        password: cryptoJS(req.body.password).toString()
+                    });
+        
+                    res.send({
+                        message: "User successfully registered",
+                        status: 200
+                    });
+        
+                } else {
+                    res.send({
+                        message: "One or more fields are incorrect",
+                        status: 406
+                    });
+                }
+        
+            });
+
+        }
+
     });
 
 });
