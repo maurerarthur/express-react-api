5855dcf0fc0eba6e6727e406f1708aca1734700c
Arthur Maurer <30410972+maurerarthur@users.noreply.github.com>
2020-09-25 21:57:06 -0300

    New files created in the project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
+import Dashboard from './dashboard/index.js';
 
-function App() {
+export default function App() {
 
-  axios.get("http://localhost:8000/api").then(res => {
-    console.log(res.data);
-  });
+  	axios.get("http://localhost:8000/api").then(res => {
+		console.log(res.data);
+  	});
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return(
+		<div className="App w-100 h-100">
+			<Dashboard />
+		</div>
+  	);
 
 }
-
-export default App;
---
+++ frontend/src/users/createAccount.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../App.css';
+
+export default function CreateAccount() {
+
+    return(
+        <div>Account creation here</div>
+    );
+
+}
