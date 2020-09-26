dfd3d7c2739a48ad637c9ded02790c7f134008c3
Arthur Maurer <30410972+maurerarthur@users.noreply.github.com>
2020-09-25 22:58:52 -0300

    Account creation HTML setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,13 +5,13 @@ import CreateAccount from './users/createAccount.js';
 
 export default function App() {
 
-  	axios.get("http://localhost:8000/api").then(res => {
+  	axios.get("http://localhost:8000/api/users").then(res => {
 		console.log(res.data);
   	});
 
 	return(
-		<div className="App w-100 h-100">
-			<CreateAccount />
+		<div className="App w-100 h-100 d-flex align-items-center justify-content-center">
+			<CreateAccount className="w-100 h-100" />
 		</div>
   	);
 
--- frontend/src/users/createAccount.js
+++ frontend/src/users/createAccount.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import '../App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function CreateAccount() {
 
     return(
-        <div>Account creation here</div>
+        <div className="container">
+            <div className="form-group">
+                <label>Username</label>
+                <input type="text" className="form-control" placeholder="Username" />
+            </div>
+            <div className="form-group">
+                <label>Email Address</label>
+                <input type="email" className="form-control" placeholder="Email" />
+            </div>
+            <div className="form-group">
+                <label>Password</label>
+                <input type="password" className="form-control" placeholder="Password" />
+            </div>
+            <button type="submit" className="btn btn-primary w-100">Submit</button>
+        </div>
     );
 
 }
